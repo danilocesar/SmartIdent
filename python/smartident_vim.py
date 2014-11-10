e22b981aff53a997cccc123f2b37713ff92180f4
Danilo Cesar Lemes de Paula <danilo.eu@gmail.com>
2014-11-10 21:50:25 -0200

    Identifying tabs

--- python/smartident_vim.py
+++ python/smartident_vim.py
@@ -10,4 +10,10 @@ SPACE = 1
 TAB = 2
 
 
-print utils.processFile(filename)
+tabs, spaces, lines = utils.processFile(filename)
+print lines, tabs
+
+# define that tab-indent files should have more than 50% of lines
+# starting with tabs
+if (tabs / float(lines) > 0.5):
+    print "USE TABS"
